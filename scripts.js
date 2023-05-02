633f49e11cbf469393bab5e88425bec13bbb94e5
SvVov <96830499+SvVov@users.noreply.github.com>
2023-05-03 00:02:20 +0300

    styled front card; added audio

--- scripts.js
+++ scripts.js
@@ -8,9 +8,9 @@
 
     function getData() {
         return [
-            { title: "this is a cat", picturePath: "user1.jpg", audioTitle: "may"},
-            { title: "this is a dog", picturePath: "user2.jpg", audioTitle: "gav"},
-            { title: "this is a bird", picturePath: "user3.jpg", audioTitle: "pii"}
+            { title: "A cat", picturePath: "cat.jpg", audioTitle: "may", audiopath: "cat.mp3"},
+            { title: "A dog", picturePath: "dog.jpg", audioTitle: "gav", audiopath: "dog.mp3"},
+            { title: "A bird", picturePath: "bird.jpg", audioTitle: "pii", audiopath: "bird.mp3"}
         ]
     }
 
@@ -20,24 +20,50 @@
 
         const swiperWrapper = document.getElementById('swiper-wrapper-id');
         for(var i = 0; i < cards.length; i++) {
-            swiperWrapper.innerHTML += getSwiperSlide(cards[i].title, cards[i].picturePath, cards[i].audioTitle);
+            swiperWrapper.innerHTML += getSwiperSlideHTML(cards[i].title, cards[i].picturePath, cards[i].audioTitle, cards[i].audiopath);
+
+            const playControl = document.getElementById(`play_${cards[i].title}_${cards[i].picturePath}`);
+            playControl.addEventListener("click", onPlayClick);
+            playControl.Card = cards[i];
         }
     }
 
-    function getSwiperSlide(title, picturePath, audioTitle, audiopath) {
+    function getSwiperSlideHTML(title, picturePath, audioTitle, audiopath) {
         return `<div class="swiper-slide">
+                    <audio id="audio_${title}_${picturePath}">
+                        <source src="audio/${audiopath}" type="audio/mpeg">
+                        Your browser does not support the audio element.
+                    </audio>
                     <div class="testmonialBox">
                         <div class="content">
-                            <p class="slide-title">${title}</p>
-                            <div class="img">
-                                <img src="imgs/${picturePath}" width="200px" height="200px">
+                            <div class="slide-title">${title}</div>
+                            <div class="slide-img">
+                                <img src="imgs/${picturePath}">
                             </div>
-                            <div class="audio">
-                                <p class="slide-audio-title">${audioTitle}</p>
+                            <div class="slide-audio-title">${audioTitle.toUpperCase()}</div>
+                            <div class="slide-controls">
+                                <div class="slide-left-control">
+                                    <img src="icons/home.svg"> 
+                                </div>
+                                <div class="slide-middle-control" id="play_${title}_${picturePath}">
+                                    <img src="icons/play.svg">
+                                </div>
+                                <div class="slide-right-control">
+                                    <img src="icons/notes.svg">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>`;
+    }
+
+    function onPlayClick(event) {
+        if (!event.currentTarget.Card) 
+            return;
+
+        const card = event.currentTarget.Card;
+        var audioElement = document.getElementById(`audio_${card.title}_${card.picturePath}`);
+        audioElement.play();
     }
 
     function initSwiper() {
@@ -54,6 +80,25 @@
                 slideShadows : false
             },
             loop: true,
+            keyboard: {
+                enabled: true,
+            }
         });
+
+        // var swiper = new Swiper('.swiper-container', {
+        //     slidesPerView: 1,
+        //     spaceBetween: 30,
+        //     keyboard: {
+        //       enabled: true,
+        //     },
+        //     pagination: {
+        //       el: '.swiper-pagination',
+        //       clickable: true,
+        //     },
+        //     navigation: {
+        //       nextEl: '.swiper-button-next',
+        //       prevEl: '.swiper-button-prev',
+        //     },
+        //   });
     }
 })();
